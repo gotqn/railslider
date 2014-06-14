f996e64f41a1f7f1ee4e05aa838e59245983b2fa
gotqn <george_27@abv.bg>
2014-06-14 16:36:25 +0300

    Initial commit

--- lib/generators/templates/railslider.rb
+++ lib/generators/templates/railslider.rb
@@ -6,7 +6,7 @@ class <%= get_class_name %> < Railslider::Image
     for index in 1 .. 7
       @result_html +=
           "
-        <div class='<%=\"rs-multi-flip rs-card rs-flip#{index}\"%>'>
+        <div class=\"rs-multi-flip rs-card rs-flip#{index}\">
           <div class=\"rs-front\"></div>
           <div class=\"rs-back\"></div>
         </div>
