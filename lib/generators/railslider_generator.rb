9ff0d728b1f0cd21ec5315ab2fbb931aa47f86c7
gotqn <george_27@abv.bg>
2014-06-14 21:19:29 +0300

    Initial commit

--- lib/generators/railslider_generator.rb
+++ lib/generators/railslider_generator.rb
@@ -6,16 +6,10 @@ class RailsliderGenerator < Rails::Generators::Base
   argument :name, type: 'string', required: true,
            desc: 'specifies generated ruby class name'
 
-  class_option :width, type: 'numeric', default: 329, aliases: '-w',
-               desc: 'specifies image width'
-
-  class_option :height, type: 'numeric', default: 425, aliases: '-h',
-               desc: 'specifies image height'
-
   def generate_layout
     template 'railslider.rb',
              "app/railslider/#{get_file_name}.rb"
-    template 'transitions.css.sass.erb',
+    template 'transitions.css.sass',
              "vendor/assets/stylesheets/railslider/#{get_file_name}/transitions.css.sass"
   end
 
@@ -27,14 +21,6 @@ class RailsliderGenerator < Rails::Generators::Base
 
   def get_class_name
     name.camelize
-  end
-
-  def get_width
-    options[:width]
-  end
-
-  def get_height
-    options[:height]
   end
 
 end
